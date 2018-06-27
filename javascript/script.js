14015e59e325b6bbdbd86af11e2f1c9e1b4f5f95
JLasc <lascaze.j@gmail.com>
2018-06-27 14:17:06 -0400

    removed object, added wordbank into array

--- javascript/script.js
+++ javascript/script.js
@@ -2,35 +2,42 @@
 
 //Global Variables
 
-var wordBank = {
-    alphabet: "abcdefghijklmnopqrstuvwxyz",
-    a: "afraid, afterlife, alarming, alien, angel, apparition, astronaut, autumn",
-    b: "ballerina, bat, beast, bizarre, black, black-cat, blood, bogeyman, bone, boo, broomstick",
-    c: "cackle,cadaver,candy,cape,carve,casket,cat,cauldron,cemetery,chilling,cloak,clown,cobweb,coffin,corpse,costume,creepy,crown,crypt",
-    d: "dark,darkness,dead,demon,devil,devilish,disguise,dreadful,death,dress-up",
-    e: "eerie,elf,enchant,evil,eyeballs,eyepatch",
-    f: "face paint, fairy, fangs, fantasy, fear, flashlight, fog, fright, frighten, frightening, frightful",
-    g: "genie, ghastly, ghost, ghoul, goblin, goodies, gory, gravestone, grim reaper, grisly, gruesome",
-    h: "hair-raising, Halloween, hat, haunt, haunted house, hayride, headstone, hobgoblin, hocus pocus, horrible, horrify, howl",
-    i: "imp",
-    l: "lantern",
-    m: "macabre,make-believe,magic,mask,masquerade,mausoleum,midnight,mist,monster,moon,morbid,mummy,mysterious",
-    n: "night,nightmare",
-    o: "october,ogre,orange,otherworldly,owl",
-    p: "party,petrify,phantasm,phantom,pitchfork,poltergeist,potion,prank,pretend,pumpkin",
-    r: "repulsive,revolting,rip,robe",
-    s: "scare, scarecrow, scream, shadow, shock, skeleton, skull, specter, spell, spider web, spine-chilling, spirit, spooky, startling, strange, supernatural, superstition, sweets",
-    t: "tarantula,terrible,terrify,thrilling,tiara,toga,tomb,treat,trick,trick-or-treat,troll",
-    u: "unearthly,unnerving",
-    v: "vampire,vanish",
-    w: "wand,warlock,web,weird,werewolf,wicked,wig,witch,witchcraft,wizard,wraith",
-    z: "zombie"
+compArray = [];
+userArray = [];
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+//Word Bank Object - 22 keys, 21 word strings
+var wordBank = "afraid, afterlife, alarming, alien, angel, apparition, astronaut, autumn, ballerina, bat, beast, bizarre, black, black cat, blood, bloodcurdling, bogeyman, bone, boo, broomstick, cackle, cadaver, candy, cape, carve, casket, cat, cauldron, cemetery, chilling, cloak, clown, cobweb, coffin, corpse, costume, creepy, crown, crypt, dark, darkness, dead, demon, devil, devilish, disguise, dreadful, death, dress-up, eerie, elf, enchant, evil, eyeballs, eyepatch, face paint, fairy, fangs, fantasy, fear, flashlight, fog, fright, frighten, frightening, frightful, genie, ghastly, ghost, ghoul, goblin, goodies, gory, gravestone, grim reaper, grisly, gruesome, hair-raising, halloween, hat, haunt, haunted house, hayride, headstone, hobgoblin, hocus pocus, horrible, horrify, howl, imp, lantern, macabre, make-believe, magic, mask, masquerade, mausoleum, midnight, mist, monster, moon, morbid, mummy, mysterious, night, nightmare, october, ogre, orange, otherworldly, owl, party, petrify, phantasm, phantom, pitchfork, poltergeist, potion, prank, pretend, pumpkin, repulsive, revolting, rip, robe scare, scarecrow, scream, shadow, shock, skeleton, skull, specter, spell, spider web, spine-chilling, spirit, spooky, startling, strange, supernatural, superstition, sweets, tarantula,terrible,terrify,thrilling,tiara,toga,tomb,treat,trick,trick-or-treat,troll,unearthly,unnerving,vampire,vanish,wand,warlock,web,weird,werewolf,wicked,wig,witch,witchcraft,wizard,wraith,zombie"
+
+
+var wordArray = wordBank.split(",")
+
+function compChoice () {
+    return wordArray[Math.floor(Math.random() * wordArray.length)]
+};
+
+//Function that will turn wordBank object key into array - seperated at the comma
+function wordArray(a) {
+    newArray = a.split(",")
+    testArray.push(newArray)
+    return testArray[0]
+}
+
+
+// test space
+function test () {
+    keys = Object.keys(wordBank) // Finds all the keys in object
+    rand = keys[Math.floor(Math.random() * keys.length)] // Picks a random key from object
+    banana = Object.getOwnPropertyNames(rand).sort() // Hopefully will list properties of object
+    return banana
 }
 
 
 
 
 
+//Key Up interactions
 document.onkeyup = function(event) {
     userGuess = event.key
     console.log(userGuess);
