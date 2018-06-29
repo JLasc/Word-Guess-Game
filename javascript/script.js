4fa066306062c357d7793cab7d809637cfd16b24
JLasc <lascaze.j@gmail.com>
2018-06-29 15:02:29 -0400

    addtl psuedo code 1-4, cleaned up wordBank - shortened list of words

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
+// Add a function that checks the winning condition 
+// Limit wrong guesses and decrement
+// link remaining guesses to #remguess
+// Add images? 
 
 
 //Globals
@@ -6,7 +10,7 @@ var i;
 var choice;
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 13;
 var gameOver = true;
 
 //Arrays
@@ -17,11 +21,11 @@ var userLtrs = []; //used
 
 
 //Word Banks - 169 words + Alphabet
-var wordBank = "afraid,afterlife,alarming,alien,angel,apparition,astronaut,autumn,ballerina,bat,beast,bizarre,black,black-cat,blood,bloodcurdling,bogeyman,bone,boo,broomstick,cackle,cadaver,candy,cape,carve,casket,cat,cauldron,cemetery,chilling,cloak,clown,cobweb,coffin,corpse,costume,creepy,crown,crypt,dark,darkness,dead,demon,devil,devilish,disguise,dreadful,death,dress-up,eerie,elf,enchant,evil,eyeballs,eyepatch,face paint,fairy,fangs,fantasy,fear,flashlight,fog,fright,frighten,frightening,frightful,genie,ghastly,ghost,ghoul,goblin,goodies,gory,gravestone,grim reaper,grisly,gruesome,hair-raising,halloween,hat,haunt,haunted house,hayride,headstone,hobgoblin,hocus pocus,horrible,horrify,howl,imp,lantern,macabre,make-believe,magic,mask,masquerade,mausoleum,midnight,mist,monster,moon,morbid,mummy,mysterious,night,nightmare,october,ogre,orange,otherworldly,owl,party,petrify,phantasm,phantom,pitchfork,poltergeist,potion,prank,pretend,pumpkin,repulsive,revolting,rip,robe scare,scarecrow,scream,shadow,shock,skeleton,skull,specter,spell,spider-web,spine-chilling,spirit,spooky,startling,strange,supernatural,superstition,sweets,tarantula,terrible,terrify,thrilling,tiara,toga,tomb,treat,trick,trick-or-treat,troll,unearthly,unnerving,vampire,vanish,wand,warlock,web,weird,werewolf,wicked,wig,witch,witchcraft,wizard,wraith,zombie";
+var wordBank = "avocado, asparagus, beans, broccoli, carrot, celery, cucumber, garlic, greenbean, lettuce, onion, pepper, peanut, potato, pumpkin, radish, salad, spinach, squash, tomato, yam, zucchini";
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 //Splits word banks into new arrays
-var wordArray = wordBank.split(",");
+var wordArray = wordBank.split(" ");
 var alphaArray = alphabet.split("");
 
 
@@ -44,31 +48,32 @@ function keyShadow (x, y) {
         document.getElementById("compword").innerHTML = userLtrs.join("")
 }
 
+
+// Win check function
+
+
 // --------------------------------------------------------------------------------------------
 //Key Up interactions
 document.onkeyup = function(event) {
     userGuess = event.key
     userGuess1 = event.keyCode
     console.log(userGuess, userGuess1);
-    userWord = document.getElementById("userguess");
     compWord = document.getElementById("compword");
-    shadow = keyShadow(userGuess, alphaArray)
-    correctGuess = keyShadow(userGuess, choiceArr)
-
-
+    shadow = keyShadow(userGuess, alphaArray);
+    correctGuess = keyShadow(userGuess, choiceArr);
 
     if (gameOver) {
         //User presses 'space' to start the game.
         if (userGuess1 === 32) {
             gameStart();
             gameOver = false;
+            remWord.innerHTML = "Guesses Left: " + guessesLeft;
 
         } else {
             compWord.innerHTML = "You need to press space to start."
         }
 
     }
-
 
     if (gameOver !== true) {
        
@@ -87,14 +92,13 @@ document.onkeyup = function(event) {
                 wrongLtrs.push(userGuess);
                     document.getElementById("usedletter").innerHTML = wrongLtrs.join("")
     }
+       
 }
 
 
 
 
 
-
- 
         
         /* gameStart() */
 
