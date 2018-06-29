85fba0f77241f1a15de7a1edcbcf2982a6d73f67
JLasc <lascaze.j@gmail.com>
2018-06-28 22:16:00 -0400

    complete code refactor

--- javascript/script.js
+++ javascript/script.js
@@ -1,69 +1,96 @@
 
 
-//Global Variables
+//Globals
 
-//Variables
 var i;
-var gamestart = false;
+var choice;
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var gameOver = true;
 
-//Empty Arrays comp choice and user guesses
-compArray = [];
-userArray = [];
-var wordSplit;
+var usedLtr = document.getElementById("usedletter");
 
-//Alphabet bank for user guesses
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-//Word Bank Object - 169 words
-var wordBank = "afraid,afterlife,alarming,alien,angel,apparition,astronaut,autumn,ballerina,bat,beast,bizarre,black,black-cat,blood,bloodcurdling,bogeyman,bone,boo,broomstick,cackle,cadaver,candy,cape,carve,casket,cat,cauldron,cemetery,chilling,cloak,clown,cobweb,coffin,corpse,costume,creepy,crown,crypt,dark,darknessm,dead,demon,devil,devilish,disguise,dreadful,death,dress-up,eerie,elf,enchant,evil,eyeballs,eyepatch,face paint,fairy,fangs,fantasy,fear,flashlight,fog,fright,frighten,frightening,frightful,genie,ghastly,ghost,ghoul,goblin,goodies,gory,gravestone,grim reaper,grisly,gruesome,hair-raising,halloween,hat,haunt,haunted house,hayride,headstone,hobgoblin,hocus pocus,horrible,horrify,howl,imp,lantern,macabre,make-believe,magic,mask,masquerade,mausoleum,midnight,mist,monster,moon,morbid,mummy,mysterious,night,nightmare,october,ogre,orange,otherworldly,owl,party,petrify,phantasm,phantom,pitchfork,poltergeist,potion,prank,pretend,pumpkin,repulsive,revolting,rip,robe scare,scarecrow,scream,shadow,shock,skeleton,skull,specter,spell,spider-web,spine-chilling,spirit,spooky,startling,strange,supernatural,superstition,sweets,tarantula,terrible,terrify,thrilling,tiara,toga,tomb,treat,trick,trick-or-treat,troll,unearthly,unnerving,vampire,vanish,wand,warlock,web,weird,werewolf,wicked,wig,witch,witchcraft,wizard,wraith,zombie"
+//Arrays
+var wrongLtrs = [];
+var choiceArr = [];
+var userLtrs = [];
+var correctLtrs = [];
+var gameArray = [];
 
-//Splits wordBank into an array separated by commas
-var wordArray = wordBank.split(",")
-var wordChoice = wordFunction();
-var finalWord = wordChoice
 
-//Clears compArray, chooses new word, and then adds it to compArray as a new index.
-function wordFunction () {
-    compArray=[]
-    a = wordArray[Math.floor(Math.random() * wordArray.length)]
-    compArray.push(a)
-    console.log(compArray)
-    b = compArray.join()
-    c = b.split("")
-    return wordSplit = c
+//Word Banks - 169 words
+var wordBank = "afraid,afterlife,alarming,alien,angel,apparition,astronaut,autumn,ballerina,bat,beast,bizarre,black,black-cat,blood,bloodcurdling,bogeyman,bone,boo,broomstick,cackle,cadaver,candy,cape,carve,casket,cat,cauldron,cemetery,chilling,cloak,clown,cobweb,coffin,corpse,costume,creepy,crown,crypt,dark,darkness,dead,demon,devil,devilish,disguise,dreadful,death,dress-up,eerie,elf,enchant,evil,eyeballs,eyepatch,face paint,fairy,fangs,fantasy,fear,flashlight,fog,fright,frighten,frightening,frightful,genie,ghastly,ghost,ghoul,goblin,goodies,gory,gravestone,grim reaper,grisly,gruesome,hair-raising,halloween,hat,haunt,haunted house,hayride,headstone,hobgoblin,hocus pocus,horrible,horrify,howl,imp,lantern,macabre,make-believe,magic,mask,masquerade,mausoleum,midnight,mist,monster,moon,morbid,mummy,mysterious,night,nightmare,october,ogre,orange,otherworldly,owl,party,petrify,phantasm,phantom,pitchfork,poltergeist,potion,prank,pretend,pumpkin,repulsive,revolting,rip,robe scare,scarecrow,scream,shadow,shock,skeleton,skull,specter,spell,spider-web,spine-chilling,spirit,spooky,startling,strange,supernatural,superstition,sweets,tarantula,terrible,terrify,thrilling,tiara,toga,tomb,treat,trick,trick-or-treat,troll,unearthly,unnerving,vampire,vanish,wand,warlock,web,weird,werewolf,wicked,wig,witch,witchcraft,wizard,wraith,zombie";
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+//Splits word banks into new array
+var wordArray = wordBank.split(",");
+var alphaArray = alphabet.split("");
+
+function keyShadow (x, y) {
+    return y.indexOf(x) > -1
 }
 
-//Takes word stored into compArray, and turns each letter into array items.
-/* function wordFoo () {
-    a = compArray.join()
-    b = a.split("")
-    return wordSplit = b
+ function gameStart () {
+    compWord = document.getElementById("compword");
+
+    choice = wordArray[Math.floor(Math.random() * wordArray.length)];
+    choiceArr = choice.split("");
+
+    for (i=0; i < choiceArr.length; i++) {
+        userLtrs.push(" _ ")
+    }
+
+        document.getElementById("compword").innerHTML = userLtrs.join("")
 }
- */
-//Splits alphabet into an array, separated by white-space
-var alphaArray = alphabet.split("")
-
-//Function that will choose a random word from wordArray
-function compChoice () {
-    compArray = [];
-    a = wordArray[Math.floor(Math.random() * wordArray.length)];
-    return compArray.push(a)
-};
-
-
-//Loop that turns wordSplit into underscores.
-for (i=0; i < wordSplit.length; i++) {
-    console.log("-")}
-
-
-
-
-
 
 // --------------------------------------------------------------------------------------------
 //Key Up interactions
 document.onkeyup = function(event) {
     userGuess = event.key
+    userGuess1 = event.keyCode
     console.log(userGuess);
+    userWord = document.getElementById("userguess");
+    shadow = keyShadow(userGuess, alphaArray)
+    correctGuess = keyShadow(userGuess, choiceArr)
+
+
+    if (gameOver) {
+        if (shadow === true) {
+        /*     userLtrs.push(userGuess)
+            wordDisplay = userLtrs.join("")
+            userWord.innerHTML = wordDisplay
+ */
+        if (correctGuess) {
+            console.log("this is a correct guess")
+                for (i=0; i < choiceArr.length; i++){
+                    if (userGuess === choiceArr[i]) {
+                        userLtrs[i] = userGuess
+                        compWord.innerHTML = userLtrs.join("")
+                    }
+                
+                }
+        } else {
+            console.log("this word is not in array")
+        }
+    }
+}
+
+
+
+
+
+ 
+        
+        /* gameStart() */
+
+     /*    if(choiceString.indexOf(userGuess) === -1) {
+            test = wrongLtrs.push(userGuess)
+            console.log(test) */
+    
+
+
+
 
 };
